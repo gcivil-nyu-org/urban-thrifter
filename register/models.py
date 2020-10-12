30f3b32669d5c269778754c3b7cb3e4a1946130b
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-12 14:01:29 -0400

    Created Helpseeker model

--- register/models.py
+++ register/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Helpseeker(models.Model):
+    BOROUGH_CHOICES=[
+        ('Manhattan', 'Manhattan'),
+        ('Brooklyn', 'Brooklyn'),
+        ('Queens', 'Queens'),
+        ('The Bronx', 'The Bronx'),
+        ('Staten Island', 'Staten Island'),
+    ]
+    RESOURCE_CHOICES=[
+        ('FOOD', 'Food'),
+        ('PPE', 'Personal Protective Equipment'),
+        ('CLOTH', 'Clothing And Covers'),
+    ]
+    
+    username = models.CharField(max_length=50, unique=True, blank=False)
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True, blank=False)
+    password = models.CharField(max_length=128, blank=False)
+    borough = models.CharField(max_length=13, choices=BOROUGH_CHOICES, blank=False)
+    complaint_count = models.IntegerField(default=0, blank=False)
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    rc_1 = models.CharField(max_length=5, choices=RESOURCE_CHOICES, default=None, blank=True, null=True)
+    rc_2 = models.CharField(max_length=5, choices=RESOURCE_CHOICES, default=None, blank=True, null=True)
+    rc_3 = models.CharField(max_length=5, choices=RESOURCE_CHOICES, default=None, blank=True, null=True)
+    
+    
+
