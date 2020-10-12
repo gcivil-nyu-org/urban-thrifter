4000d5bda8c72f2474a239cec21d000118860209
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-12 17:33:04 -0400

    Added matching password and password validation

--- register/forms.py
+++ register/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Helpseeker
 from django.core.exceptions import ValidationError
+from django.contrib.auth import password_validation
 
 BOROUGH_CHOICES=[
     ('Manhattan', 'Manhattan'),
@@ -20,6 +21,7 @@ class HelpseekerForm(forms.ModelForm):
     username = forms.CharField(label='Username', min_length=4, max_length=50, required=True)
     email = forms.EmailField(label='Email', max_length=60, required=True)
     password = forms.CharField(label='Password', max_length=30, widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(label='Confirm Password', max_length=30, widget=forms.PasswordInput, required=True)
     borough = forms.CharField(label='Borough', widget=forms.RadioSelect(choices=BOROUGH_CHOICES), required=True)
     resource = forms.MultipleChoiceField(label='Resources (select up to 3)', widget=forms.CheckboxSelectMultiple, choices=RESOURCE_CHOICES, required=False)
 
@@ -28,6 +30,8 @@ class HelpseekerForm(forms.ModelForm):
         holder = Helpseeker.objects.filter(username=username)
         if holder.count():
             raise ValidationError("Username already exists")
+        if not username.isalnum():
+            raise ValidationError("Username must contain only letters and numbers")
         return username
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
@@ -35,6 +39,13 @@ class HelpseekerForm(forms.ModelForm):
         if holder.count():
             raise ValidationError("Email already exists")
         return email
+    def clean_password2(self):
+        password = self.cleaned_data['password']
+        password_validation.validate_password(password)
+        password2 = self.cleaned_data['password2']
+        if password and password2 and password != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
     def clean_resource(self):
         resource = self.cleaned_data['resource']
         resource_length = len(resource)
@@ -44,7 +55,9 @@ class HelpseekerForm(forms.ModelForm):
 
     class Meta:
         model = Helpseeker
-        fields = ('username',  'email', 'password', 'borough')
+        fields = ('username', 'email', 'password', 'borough')
+
+    field_order = ['username', 'email', 'password', 'password2', 'borough', 'resource']
         
 
     
--- register/views.py
+++ register/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import HelpseekerForm
