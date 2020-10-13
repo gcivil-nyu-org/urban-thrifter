22bca056b0f1a39559563c786de7b2efe0924fd6
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-10-13 16:34:34 -0400

    fix debug value

--- urban_thrifter/settings.py
+++ urban_thrifter/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DJANGO_DEBUG_VALUE') == 'True')
-DEBUG = 'True'
+DEBUG = (os.environ.get('DJANGO_DEBUG_VALUE') == 'True')
+# DEBUG = 'True'
 
 ALLOWED_HOSTS = ['localhost', 'urban-thrifter.herokuapp.com']
 
