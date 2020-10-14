f59b60a6f470d44fcfdfe05c8f834cc2242e9162
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-14 12:11:30 -0400

    Testing git ignore

---
+++ register/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name="register"
+urlpatterns = [
+    path('', views.register, name="register"),
+    path('helpseeker', views.helpseeker_register, name="helpseeker_register"),
+    path('donor', views.donor_register, name="donor_register"),
+    path('activate/<uidb64>/<token>', views.activate_account, name="activate"),
+    path('email_sent', views.email_sent, name="email_sent"),
+]
