64eccc62991fa7d6e22c27dae787eef89bfbd0f1
Rahul Garg <53116710+rahulgarg97@users.noreply.github.com>
2020-10-14 11:31:43 -0400

    Update urls.py

--- register/urls.py
+++ register/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name="register"
 urlpatterns = [
     path('', views.register, name="register"),
-    path('helpseeker_register', views.helpseeker_register, name="helpseeker_register"),
-    path('donor_register', views.donor_register, name="donor_register"),
+    path('helpseeker', views.helpseeker_register, name="helpseeker_register"),
+    path('donor', views.donor_register, name="donor_register"),
     path('activate/<uidb64>/<token>', views.activate_account, name="activate"),
     path('email_sent', views.email_sent, name="email_sent"),
 ]
