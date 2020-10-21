aee48c6c03b40676241e719b5d44e4b504f409cb
Lynn Li <leelightman@gmail.com>
2020-10-20 20:02:46 -0400

    css update

--- donation/views.py
+++ donation/views.py
@@ -12,8 +12,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 def home(request):
     context = {
-        #'posts': posts,
-        #'title': 'ALL POSTS'
         'posts': ResourcePost.objects.all()
     }
 
