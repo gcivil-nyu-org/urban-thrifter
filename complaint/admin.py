edbdffddb1ae629c45b31235e50a45f4d02986f0
Lynn Li <leelightman@gmail.com>
2020-10-22 18:35:13 -0400

    black and flake8

--- complaint/admin.py
+++ complaint/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Complaint
 
-class ComplaintAdmin(admin.ModelAdmin):
-    readonly_fields = ('uploaded_at','subject','message','image')
 
-admin.site.register(Complaint,ComplaintAdmin)
+class ComplaintAdmin(admin.ModelAdmin):
+    readonly_fields = ("uploaded_at", "subject", "message", "image")
+
+
+admin.site.register(Complaint, ComplaintAdmin)
