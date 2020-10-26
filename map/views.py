11dd391fc130919cb5c7a092bd813e67dde40b37
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-10-25 23:04:15 -0400

    add filter css

--- map/views.py
+++ map/views.py
@@ -3,8 +3,6 @@ import requests
 from django.shortcuts import render
 from django.apps import apps
 
-# Create your views here.
-
 def main_map(request):
     resource_post_model = apps.get_model('donation', 'ResourcePost') #getting model from donation app
     post_context = {
@@ -17,7 +15,18 @@ def main_map(request):
     drop_in_center_URL = "https://data.cityofnewyork.us/resource/bmxf-3rd4.json"
     drop_in_center_r = requests.get(url = drop_in_center_URL)
     drop_in_centers = drop_in_center_r.json()
-    # Information in JSON
+
+    for center in drop_in_centers:
+        new_address = ""
+        for part in center["address"].split():
+            new_address = new_address + " " + part
+            if new_address[-1] == ";" or new_address[-2:] ==".,":
+                center["address"] = new_address[:-1]
+                break
+                
+    return render(request, 'map/main.html', { 'mapbox_access_token': mapbox_access_token, 'drop_in_centers': drop_in_centers, 'post_context': post_context['resource_posts'] } )
+
+    # Information in drop_in_centers
     # [{'center_name': 'Living Room',
     #   'borough': 'Bronx',
     #   'address': '800 Barretto Street; Bronx, NY\n10474',
@@ -31,15 +40,3 @@ def main_map(request):
     #   'bin': '2006002',
     #   'bbl': '2027400100',
     #   'nta': 'Hunts Point'},
-    for center in drop_in_centers:
-        new_address = ""
-        for part in center["address"].split():
-            new_address = new_address + " " + part
-            if new_address[-1] == ";" or new_address[-2:] ==".,":
-                center["address"] = new_address[:-1]
-                break
-            else:
-                continue
-    return render(request, 'map/main.html', { 'mapbox_access_token': mapbox_access_token, 'drop_in_centers': drop_in_centers, 'post_context': post_context['resource_posts'] } )
-
-    
