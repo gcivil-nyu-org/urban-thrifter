e03b97f0c9140bfb3deff32ca6476c73b1a3b738
Sesha Sai Sreevani Kappagantula <kvaniammalu.14@gmail.com>
2020-10-26 14:29:24 -0400

    just removed spaces

--- register/urls.py
+++ register/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path("donor", views.donor_register, name="donor_register"),
     path("activate/<uidb64>/<token>", views.activate_account, name="activate"),
     path("email_sent", views.email_sent, name="email_sent"),
-    path(
-        "helpseeker/profile/", views.helpseeker_edit_profile, name="helpseeker_profile"
+    path("helpseeker/profile/", views.helpseeker_edit_profile, name="helpseeker_profile"
     ),
 ]
