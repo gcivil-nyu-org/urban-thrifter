38efa7746b7d6a88198dfb0ebed38a51e3c516cf
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-10-27 19:35:40 -0400

    test map and view changes

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,18 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class MapConfigTest(TestCase):
+    def test_apps(self):
+        self.assertEqual(SigninConfig.name, "map")
+
+class BaseTest(TestCase):
+    
+    def setUp(self):
+        self.map_url = reverse("map:main-map")
+
+class RunningTest(BaseTest):
+
+    def test_can_view_map_page(self):
+        response = self.client.get(self.map_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "map/main.html")
