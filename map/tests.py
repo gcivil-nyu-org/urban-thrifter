3794d3d4a2925787130f6c08797e36fd4883a2a5
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-10-27 19:59:03 -0400

    change test

--- map/tests.py
+++ map/tests.py
@@ -1,18 +1,24 @@
 from django.test import TestCase
 from django.urls import reverse
+from map.apps import MapConfig
+from django.test import Client
+
 
 class MapConfigTest(TestCase):
     def test_apps(self):
-        self.assertEqual(SigninConfig.name, "map")
+        self.assertEqual(MapConfig.name, "map")
 
-class BaseTest(TestCase):
-    
-    def setUp(self):
-        self.map_url = reverse("map:main-map")
+class GetTest(TestCase):
 
-class RunningTest(BaseTest):
+    @classmethod
+    def setUpClass(self):
+        # creating instance of a client.
+        self.client = Client()
 
-    def test_can_view_map_page(self):
-        response = self.client.get(self.map_url)
+    def test_getLogin(self):
+        # Issue a GET request.
+        response = self.client.get('/map/')
+
+        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "map/main.html")
+
