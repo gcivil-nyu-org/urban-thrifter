a2208d121723f82138c3b379c774a9254bc45697
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-28 01:44:42 -0400

    Added form wrong and right tests

--- register/tests.py
+++ register/tests.py
@@ -21,5 +21,82 @@ class HelpseekerRegistrationTests(TestCase):
     def test_resource_label(self):
         form = HelpseekerForm()
         self.assertTrue(
-            form.fields["resource"].label == "Resources (Optional, select up to 3)"
+            form.fields["resource"].label == "Resources (Optional, select up to 3)")
+
+    def test_form_working(self):
+        form = HelpseekerForm(
+            data={
+                "username": "Jonathan",
+                "email": "ponathanjun@gmail.com",
+                "password1": "peaches12",
+                "password2": "peaches12",
+                "borough": "MAN",
+                "resource": ["FOOD", "MDCL"],
+            }
         )
+        self.assertTrue(form.is_valid())
+
+    def test_form_username_wrong(self):
+        form = HelpseekerForm(
+            data={
+                "username": "jon",
+                "email": "ponathanjun@gmail.com",
+                "password1": "peaches12",
+                "password2": "peaches12",
+                "borough": "MAN",
+                "resource": ["FOOD", "MDCL"],
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_form_email_wrong(self):
+        form = HelpseekerForm(
+            data={
+                "username": "Jonathan",
+                "email": "ponathanjun",
+                "password1": "peaches12",
+                "password2": "peaches12",
+                "borough": "MAN",
+                "resource": ["FOOD", "MDCL"],
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_form_password_wrong(self):
+        form = HelpseekerForm(
+            data={
+                "username": "Jonathan",
+                "email": "ponathanjun@gmail.com",
+                "password1": "dog",
+                "password2": "dog",
+                "borough": "MAN",
+                "resource": ["FOOD", "MDCL"],
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_form_password_match_wrong(self):
+        form = HelpseekerForm(
+            data={
+                "username": "Jonathan",
+                "email": "ponathanjun@gmail.com",
+                "password1": "peaches12",
+                "password2": "peaches13",
+                "borough": "MAN",
+                "resource": ["FOOD", "MDCL"],
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_form_resource_wrong(self):
+        form = HelpseekerForm(
+            data={
+                "username": "Jonathan",
+                "email": "ponathanjun@gmail.com",
+                "password1": "peaches12",
+                "password2": "peaches12",
+                "borough": "MAN",
+                "resource": ["dogs", "cats"],
+            }
+        )
+        self.assertFalse(form.is_valid())
