d983717dd4161847806880a1a622744137a87b52
Lynn Li <leelightman@gmail.com>
2020-10-28 12:50:35 -0400

    update donation test

--- donation/tests.py
+++ donation/tests.py
@@ -2,8 +2,20 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import ResourcePost
 from django.utils import timezone
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+import tempfile
+from django.test import override_settings
 
 # Create your tests here.
+
+
+def get_temporary_image(temp_file):
+    size = (200, 200)
+    color = (255, 0, 0, 0)
+    image = Image.new("RGB", size, color)
+    image.save(temp_file, "jpeg")
+    return temp_file
 
 
 class ResourcePostCreateViewTests(TestCase):
@@ -47,6 +59,31 @@ class ResourcePostCreateViewTests(TestCase):
         )
         self.assertTrue(create_post.check_quantity())
 
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    def test_image_crop(self):
+        # temp_file = tempfile.NamedTemporaryFile()
+        # image = get_temporary_image(temp_file)
+        small_gif = (
+            b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04"
+            b"\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02"
+            b"\x02\x4c\x01\x00\x3b"
+        )
+        image = SimpleUploadedFile("small.gif", small_gif, content_type="image/gif")
+        create_resource_post = ResourcePost(
+            title="test",
+            description="test",
+            quantity=10,
+            dropoff_time_1=timezone.now(),
+            date_created=timezone.now(),
+            resource_category="FOOD",
+            image=image,
+            status="AVAILABLE",
+        )
+        create_resource_post.save()
+        url = reverse("donation-detail", args=(create_resource_post.pk,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
 
 class ResourcePostListViewTests(TestCase):
     def test_no_post(self):
@@ -65,11 +102,18 @@ class ResourcePostListViewTests(TestCase):
 
 
 class HomepageViewTests(TestCase):
-    def test_homepage(self):
+    def test_reservation(self):
         """
         If no post exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        """
+        If no post exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("reservation"))
         self.assertEqual(response.status_code, 200)
 
 
