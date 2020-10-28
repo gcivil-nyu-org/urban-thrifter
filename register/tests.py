ed300dfe410cbde52aaacdbb3a41f11d6c4e8250
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-28 01:43:12 -0400

    Added helpseeker registration form label tests

--- register/tests.py
+++ register/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-from .models import HelpseekerProfile
+from .forms import HelpseekerForm
 
+class HelpseekerRegistrationTests(TestCase):
+    def test_first_name_label(self):
+        form = HelpseekerForm()
+        self.assertTrue(form.fields["username"].label == "Username")
+
+    def test_password_label(self):
+        form = HelpseekerForm()
+        self.assertTrue(form.fields["password1"].label == "Password")
+
+    def test_password2_label(self):
+        form = HelpseekerForm()
+        self.assertTrue(form.fields["password2"].label == "Confirm Password")
+
+    def test_borough_label(self):
+        form = HelpseekerForm()
+        self.assertTrue(form.fields["borough"].label == "Borough")
+
+    def test_resource_label(self):
+        form = HelpseekerForm()
+        self.assertTrue(
+            form.fields["resource"].label == "Resources (Optional, select up to 3)"
+        )
