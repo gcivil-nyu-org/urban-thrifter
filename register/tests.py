d9eb18debb2f0f0732388dedabf1c8e87a452cc0
Jonathan Pun <jonathan.pun88@gmail.com>
2020-10-28 01:39:34 -0400

    Removed one test

--- register/tests.py
+++ register/tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
 from .models import HelpseekerProfile
 
-
-class HelpseekerProfileModelTests(TestCase):
-    def test_check_for_null_borough(self):
-        boro = None
-        profileWithNoBorough = HelpseekerProfile(borough=boro)
-        self.assertIs(profileWithNoBorough.has_borough(), False)
