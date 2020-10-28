bb4ce5ca025b044aada188c42b3d030d0fa17992
Rahul <rahulgarg0697@gmail.com>
2020-10-27 21:06:07 -0400

    add login redirect in settings

--- urban_thrifter/settings.py
+++ urban_thrifter/settings.py
@@ -162,6 +162,7 @@ EMAIL_PORT = 587
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
+LOGIN_REDIRECT_URL = "home"
 
 AXES_FAILURE_LIMIT = 5
 AXES_COOLOFF_TIME = datetime.timedelta(minutes=10)
