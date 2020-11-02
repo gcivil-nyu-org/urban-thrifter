54fd2cbf17e0270348ddbb35abce155625f08944
Rahul <rahulgarg0697@gmail.com>
2020-11-02 12:44:41 -0500

    add foreign key to user model

--- donation/models.py
+++ donation/models.py
@@ -7,6 +7,9 @@ from PIL import Image
 
 # from django.contrib.auth.models import User
 from places.fields import PlacesField
+from register.models import DonorProfile
+from django.contrib.auth.models import User
+
 
 
 # Create your models here.
@@ -64,7 +67,8 @@ class ResourcePost(models.Model):
     dropoff_time_2 = models.DateTimeField(blank=True, null=True)
     dropoff_time_3 = models.DateTimeField(blank=True, null=True)
     date_created = models.DateTimeField(default=timezone.now)
-
+    donor = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     # donor_id = models.ForeignKey(User, on_delete=models.CASCADE)
     # 1:n relationship
 
