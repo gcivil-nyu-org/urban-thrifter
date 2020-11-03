1dc441446304a42bce486d03b182a6f269712408
Sesha Sai Sreevani Kappagantula <kvaniammalu.14@gmail.com>
2020-11-03 10:32:18 -0500

    changed render

--- map/views.py
+++ map/views.py
@@ -86,7 +86,6 @@ def main_map(request):
             "internet_centers": internet_centers,
             "computer_centers": computer_centers,
             "after_school_prgms":after_school_prgms,
-            "public_toilets":public_toilets,
             "shelter_geojson": shelter_geojson,
         },
     )
