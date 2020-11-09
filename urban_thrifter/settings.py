eb893dbaf500a3f8446d98827595425885dd9461
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-11-09 13:57:47 -0500

    update aws region

--- urban_thrifter/settings.py
+++ urban_thrifter/settings.py
@@ -188,6 +188,9 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = False #keep all file names distinct
 AWS_DEFAULT_ACL = None
 
+AWS_S3_REGION_NAME = 'us-east-2' #change to your region
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals(), test_runner=False)
