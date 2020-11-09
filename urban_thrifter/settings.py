3bb641e5eab7cc6b42573b44beb5febb0ce879da
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-11-09 14:04:12 -0500

    add some commnets on setting

--- urban_thrifter/settings.py
+++ urban_thrifter/settings.py
@@ -181,6 +181,7 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
 
+# For AWS S3
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
@@ -188,9 +189,11 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = False #keep all file names distinct
 AWS_DEFAULT_ACL = None
 
+# https://github.com/jschneier/django-storages/issues/687
 AWS_S3_REGION_NAME = 'us-east-2' #change to your region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# End For AWS S3
 
 django_heroku.settings(locals(), test_runner=False)
