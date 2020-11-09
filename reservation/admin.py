047eb589a5e1c5151966ec228abdefd2f031d30b
Rahul <rahulgarg0697@gmail.com>
2020-11-08 22:43:28 -0500

    register Notification model in admin

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ReservationPost
+from .models import ReservationPost, Notification
 
 
 # Register your models here.
 admin.site.register(ReservationPost)
+admin.site.register(Notification)
