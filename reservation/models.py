845d8e50b167f8fe1bf79736bb83a91aff26ccf7
Sesha Sai Sreevani Kappagantula <kvaniammalu.14@gmail.com>
2020-11-16 15:58:16 -0500

    Update models.py

--- reservation/models.py
+++ reservation/models.py
@@ -44,7 +44,7 @@ class ReservationPost(models.Model):
 
 class Notification(models.Model):
     NOTIFICATION_STATUS = ((1, "ACCEPT"), (2, "REJECT"), (3, "PENDING"))
-
+    helpseeker_noti_objects=[]
     post = models.ForeignKey(
         ReservationPost,
         on_delete=models.CASCADE,
@@ -63,7 +63,7 @@ class Notification(models.Model):
     )
     date = models.DateTimeField(auto_now_add=True)
     is_seen = models.BooleanField(default=False)
-
+    msg=models.CharField(default="Hi",blank=True, null=True, max_length=50)
     __original_notificationstatus = None
 
     def __init__(self, *args, **kwargs):
@@ -73,16 +73,27 @@ class Notification(models.Model):
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
         if self.notificationstatus != self.__original_notificationstatus:
             # name changed - do something here
+            Notification.helpseeker_noti_objects.append(self)
             print("hello")
-            template = loader.get_template("reservation/messages.html")
-
-            context = {
-                    "notification": self,
-                }
-
-            return HttpResponse(template.render(context))
 
         super(Notification, self).save(force_insert, force_update, *args, **kwargs)
         self.__original_notificationstatus = self.notificationstatus
+
+'''class NotificationHelpseeker(models.Model):
+    NOTIFICATION_STATUS = ((1, "ACCEPTED"), (2, "REJECTED"), (3, "CANCELLED"))
+
+    post = models.ForeignKey(
+        ReservationPost,
+        on_delete=models.CASCADE,
+        related_name="noti_post",
+        blank=True,
+        null=True,
+    )
+    notificationstatus = models.IntegerField(
+        choices=NOTIFICATION_STATUS, default=NOTIFICATION_STATUS[2][0]
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    is_seen = models.BooleanField(default=False)
+'''
 
 post_save.connect(ReservationPost.give_notifications, sender=ReservationPost)
