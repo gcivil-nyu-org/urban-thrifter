bedb26d6a1a9e148e8f8af2079280d1e6cd09b52
Jonathan Pun <jonathan.pun88@gmail.com>
2020-11-19 17:35:15 -0500

    Trying to fix Travis once again

--- reservation/views.py
+++ reservation/views.py
@@ -48,7 +48,7 @@ def donation_post_list(request):
     reservation_pending_list = reservation_list.filter(
         post__status__in=["Pending", "PENDING"]
     )
-    print(reservation_pending_list)
+    # print(reservation_pending_list)
     reservation_closed_list = reservation_list.filter(
         post__status__in=["Closed", "CLOSED"]
     )
