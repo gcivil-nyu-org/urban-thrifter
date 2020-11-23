928851aa94317b356e62356156437888dc46a148
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-11-23 16:08:51 -0500

    add issuer, receiver

--- complaint/models.py
+++ complaint/models.py
@@ -17,4 +17,4 @@ class Complaint(models.Model):
     image = models.ImageField(upload_to="images/", null=True, blank=True)
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="Pending")
     issuer = models.ForeignKey(User, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    # resource_post of his/her
