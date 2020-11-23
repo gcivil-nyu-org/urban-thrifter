040e3b3f2523c56cda46c8517b227e6c2ecf024c
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-11-23 11:57:17 -0500

    add field

--- complaint/models.py
+++ complaint/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 
+
+STATUS_CHOICES = (
+    ("AVAILABLE", "Available"),
+    ("RESERVED", "Reserved"),
+    ("PENDING", "Pending"),
+    ("CLOSED", "Closed"),
+)
 class Complaint(models.Model):
     subject = models.CharField(max_length=100)
     message = models.CharField(max_length=100)
     uploaded_at = models.DateTimeField(auto_now_add=True, null=True)
     image = models.ImageField(upload_to="images/", null=True, blank=True)
+    status = models.CharField(
+        max_length=100, choices=STATUS_CHOICES, default="Available"
+    )
