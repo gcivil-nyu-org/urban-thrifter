2972584a6a94b69f2ce278cb3ccccc110757b967
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-11-23 14:29:43 -0500

    add issuer, receiver

--- complaint/models.py
+++ complaint/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
 STATUS_CHOICES = (
-    ("AVAILABLE", "Available"),
-    ("RESERVED", "Reserved"),
+    ("VALID", "Valid"),
     ("PENDING", "Pending"),
-    ("CLOSED", "Closed"),
+    ("INVALID", "Invlaid"),
 )
+
+
 class Complaint(models.Model):
     subject = models.CharField(max_length=100)
     message = models.CharField(max_length=100)
     uploaded_at = models.DateTimeField(auto_now_add=True, null=True)
     image = models.ImageField(upload_to="images/", null=True, blank=True)
-    status = models.CharField(
-        max_length=100, choices=STATUS_CHOICES, default="Available"
-    )
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="Pending")
+    issuer = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
