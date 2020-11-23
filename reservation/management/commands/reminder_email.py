4c79ae2878846fbc5dde485c346085cba8f8abd6
Rahul <rahulgarg0697@gmail.com>
2020-11-23 18:03:13 -0500

    add reminder email custome command

--- reservation/management/commands/reminder_email.py
+++ reservation/management/commands/reminder_email.py
@@ -1,20 +1,50 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from reservation.models import ReservationPost, Notification
-from register.models import DonorProfile
+from reservation.models import ReservationPost
+import datetime
+import pytz
+from django.core.mail import send_mail
 
-class ReminderEmail(BaseCommand):
-    help = 'Send reminder emails to donors before 15 minutes of the actual donation time'
 
-    def handle(self):
+class Command(BaseCommand):
+    help = (
+        "Send reminder emails to donors before 15 minutes of the actual donation time"
+    )
+
+    def handle(self, *args, **options):
         try:
-            notifications = Notification.objects.filter(notificationstatus='ACCEPT', post.Exists(DonorProfile.objects.filter(sender__eq=OuterRef('pk'))))
-            # User.objects.filter(Exists(Reports.objects.filter(user__eq=OuterRef('pk'))))
-            for resourcepost in resourceposts:
-                 if 
-        except FieldDoesNotExist:
-            self.stdout.write(self.style.ERROR('Field "title" does not exist.'))
+            acceptedposts = ReservationPost.objects.filter(
+                reservationstatus=1,
+                dropoff_time_request__gt=datetime.datetime.now(),
+                dropoff_time_request__lte=datetime.datetime.now()
+                + datetime.timedelta(minutes=10),
+            )
+            email_subject = "Reminder for your incoming donation dropoff"
+            for acceptedpost in acceptedposts:
+                print(acceptedpost.dropoff_time_request)
+                to_email = User.objects.filter(id=acceptedpost.donor.pk).values("email")
+                dropoff_time = str(
+                    acceptedpost.dropoff_time_request.astimezone(
+                        pytz.timezone("US/Eastern")
+                    ).strftime("%-I:%M")
+                )
+                message = (
+                    "Just a quick reminder. The dropoff time of your donation of "
+                    + str(acceptedpost.post.title)
+                    + " for "
+                    + str(acceptedpost.helpseeker)
+                    + " is at "
+                    + dropoff_time
+                    + " today."
+                )
+                send_mail(
+                    email_subject,
+                    message,
+                    "nyu.django.unchained@gmail.com",
+                    [to_email[0]["email"]],
+                    fail_silently=False,
+                )
+        except Exception as e:
+            print(e)
             return
-
-        self.stdout.write(self.style.SUCCESS('Successfully printed all Book titles'))
         return
--- reservation/models.py
+++ reservation/models.py
@@ -8,7 +8,7 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 class ReservationPost(models.Model):
-    Reservation_Status = ((1, "ACCEPT"), (2, "REJECT"), (3, "PENDING"))
+    Reservation_Status = ((1, "accept"), (2, "reject"), (3, "pending"))
     dropoff_time_request = models.DateTimeField(default=timezone.now)
     post = models.ForeignKey(ResourcePost, on_delete=models.CASCADE)
     donor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="donor_id")
@@ -27,6 +27,8 @@ class ReservationPost(models.Model):
 
     def give_notifications(sender, instance, *args, **kwargs):
         reservationpost = instance
+        if reservationpost.reservationstatus == 1 or reservationpost.reservationstatus == 2:
+            return
         reservepost = reservationpost.post
         helpseker = reservationpost.helpseeker
         notify = Notification(
