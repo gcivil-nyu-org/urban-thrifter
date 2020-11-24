3e2d702c5a16b85404db5d630dd7c977851ad338
Rahul <rahulgarg0697@gmail.com>
2020-11-23 21:16:09 -0500

    add URL to the reserved resource post in the email

--- reservation/management/commands/reminder_email.py
+++ reservation/management/commands/reminder_email.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from reservation.models import ReservationPost
 import datetime
 import pytz
+import os
 from django.core.mail import send_mail
 
 
@@ -17,7 +18,7 @@ class Command(BaseCommand):
                 reservationstatus=1,
                 dropoff_time_request__gt=datetime.datetime.now(),
                 dropoff_time_request__lte=datetime.datetime.now()
-                + datetime.timedelta(minutes=10),
+                + datetime.timedelta(hours=24),
             )
             email_subject = "Reminder for your incoming donation dropoff"
             for acceptedpost in acceptedposts:
@@ -28,6 +29,7 @@ class Command(BaseCommand):
                         pytz.timezone("US/Eastern")
                     ).strftime("%b %d %Y %-I:%M %p")
                 )
+                LINK = "https://" + str(os.environ.get("DOMAIN_NAME")) + "/donation/post/" + str(acceptedpost.pk)
                 message = (
                     "<h1>DONATION DROPOFF REMINDER</h1><p><h3>Just a quick reminder. The dropoff time for donation of <strong>"
                     + str(acceptedpost.post.title)
@@ -35,7 +37,7 @@ class Command(BaseCommand):
                     + str(acceptedpost.helpseeker)
                     + "</strong> is at <strong>"
                     + dropoff_time
-                    + "</strong>. </h3></p>"
+                    + "</strong>. Here is the link to your post: " + LINK + "</h3></p>"
                 )
                 send_mail(
                     email_subject,
