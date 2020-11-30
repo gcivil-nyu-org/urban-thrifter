77998d1f41efa33b1fbcfdb6571864efd3b34d95
Rahul <rahulgarg0697@gmail.com>
2020-11-29 19:33:45 -0500

    format black and flake8 reservation migration

--- reservation/migrations/0004_auto_20201123_1209.py
+++ reservation/migrations/0004_auto_20201123_1209.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reservation', '0003_auto_20201123_1203'),
+        ("reservation", "0003_auto_20201123_1203"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='reservationpost',
-            name='reservationstatus',
-            field=models.IntegerField(choices=[(1, 'accept'), (2, 'reject'), (3, 'pending')], default=3),
+            model_name="reservationpost",
+            name="reservationstatus",
+            field=models.IntegerField(
+                choices=[(1, "accept"), (2, "reject"), (3, "pending")], default=3
+            ),
         ),
     ]
