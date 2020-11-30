1a2940e50ecb5a8f0e6a9bf7c26a7f92283207ff
Sesha Sai Sreevani Kappagantula <kvaniammalu.14@gmail.com>
2020-11-30 14:05:49 -0500

    Update views.py

--- register/views.py
+++ register/views.py
@@ -156,7 +156,7 @@ class DonorUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     ):
         username = self.kwargs.get("username")
         user=User.objects.get(username=username)
-        if user.helpseekerprofile:
+        if user.donorprofile:
             raise Http404
         return get_object_or_404(DonorProfile, user__username__iexact=username)
 
