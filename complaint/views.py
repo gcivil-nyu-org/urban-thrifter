39ac3918c79eaf70cabce6ab900d61f367d0d4d0
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-11-30 18:47:16 -0500

    resource post to reservation post

--- complaint/views.py
+++ complaint/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
 from .models import Complaint
-from donation.models import ResourcePost
+from reservation.models import ReservationPost
 from django.urls import reverse
 
 @login_required
@@ -24,7 +24,7 @@ def issue_complaint(request, **kwargs):
             new_form = ComplaintForm
             final_form = filled_form.save(commit=False)
             final_form.issuer = request.user
-            final_form.reservation_post = ResourcePost.objects.get(id=kwargs['pk'])
+            final_form.reservation_post = ReservationPost.objects.get(id=kwargs['pk']) 
 
             try:
                 if request.user.helpseekerprofile:
@@ -39,20 +39,3 @@ def issue_complaint(request, **kwargs):
     else:
         form = ComplaintForm()
         return render(request, "complaint/complaint_form.html", {"form": form})
-
-
-# class ComplaintCreateView(LoginRequiredMixin, CreateView):	
-#     model = Complaint	
-#     fields = ["subject", "message", "image"]	
-#     success_message = "Your complaint has been received. We will look into it."	
-	
-#     def form_valid(self, form):	
-#         form.instance.issuer = self.request.user	
-#         return super().form_valid(form)	
-	
-#     def get_success_url(self):	
-#         return redirect('issue-complaint')
-
-#     def get_context_data(self, **kwargs):
-#         context = super(ComplaintCreateView, self).get_context_data(**kwargs)
-#         return context
