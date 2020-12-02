590b439eec58b23343ba6aa792a7190427f696de
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-12-01 22:39:46 -0500

    add only authorized feature

--- complaint/migrations/0002_auto_20201201_1840.py
+++ complaint/migrations/0002_auto_20201201_1840.py
@@ -8,35 +8,58 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reservation', '0006_reservationpost_reservationstatus'),
+        ("reservation", "0006_reservationpost_reservationstatus"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('complaint', '0001_initial'),
+        ("complaint", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='complaint',
-            name='issuer',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='issuer_id', to=settings.AUTH_USER_MODEL),
+            model_name="complaint",
+            name="issuer",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="issuer_id",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='complaint',
-            name='receiver',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='receiver_id', to=settings.AUTH_USER_MODEL),
+            model_name="complaint",
+            name="receiver",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="receiver_id",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='complaint',
-            name='reservation_post',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='reservation.reservationpost'),
+            model_name="complaint",
+            name="reservation_post",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="reservation.reservationpost",
+            ),
         ),
         migrations.AddField(
-            model_name='complaint',
-            name='status',
-            field=models.CharField(choices=[('PENDING', 'Pending'), ('VALID', 'Valid'), ('INVALID', 'Invlaid')], default='PENDING', max_length=100),
+            model_name="complaint",
+            name="status",
+            field=models.CharField(
+                choices=[
+                    ("PENDING", "Pending"),
+                    ("VALID", "Valid"),
+                    ("INVALID", "Invlaid"),
+                ],
+                default="PENDING",
+                max_length=100,
+            ),
         ),
         migrations.AlterField(
-            model_name='complaint',
-            name='message',
+            model_name="complaint",
+            name="message",
             field=models.TextField(),
         ),
     ]
--- complaint/views.py
+++ complaint/views.py
@@ -7,32 +7,40 @@ from reservation.models import ReservationPost
 
 @login_required
 def issue_complaint(request, **kwargs):
+    user = request.user
+    reservation_post = ReservationPost.objects.get(id=kwargs["pk"])
 
-    if request.method == "POST":
-        filled_form = ComplaintForm(request.POST, request.FILES)
-        if filled_form.is_valid():
-            messages.success(
-                request,
-                'Your complaint about "%s" has been received. \
-                        We will look into it.'
-                % (filled_form.cleaned_data["subject"]),
-            )
+    if user == reservation_post.donor or user == reservation_post.helpseeker:
+        if request.method == "POST":
+            filled_form = ComplaintForm(request.POST, request.FILES)
+            if filled_form.is_valid():
+                messages.success(
+                    request,
+                    'Your complaint about "%s" has been received. \
+                            We will look into it.'
+                    % (filled_form.cleaned_data["subject"]),
+                )
 
-            new_form = ComplaintForm
-            final_form = filled_form.save(commit=False)
-            final_form.issuer = request.user
-            final_form.reservation_post = ReservationPost.objects.get(id=kwargs["pk"])
+                new_form = ComplaintForm
+                final_form = filled_form.save(commit=False)
+                final_form.issuer = user
+                final_form.reservation_post = reservation_post
 
-            try:
-                if request.user.helpseekerprofile:
-                    final_form.receiver = final_form.reservation_post.donor
-            except Exception:
-                if request.user.donorprofile:
-                    final_form.receiver = final_form.reservation_post.helpseeker
+                try:
+                    if user.helpseekerprofile:
+                        final_form.receiver = final_form.reservation_post.donor
+                except Exception:
+                    if user.donorprofile:
+                        final_form.receiver = final_form.reservation_post.helpseeker
 
-            final_form.save()
-            return render(request, "complaint/complaint_form.html", {"form": new_form})
+                final_form.save()
+                return render(
+                    request, "complaint/complaint_form.html", {"form": new_form}
+                )
 
+        else:
+            form = ComplaintForm()
+            return render(request, "complaint/complaint_form.html", {"form": form})
     else:
-        form = ComplaintForm()
-        return render(request, "complaint/complaint_form.html", {"form": form})
+        messages.warning(request, "Not an authorized user to enter this page.")
+        return render(request, "complaint/wrong_user.html")
