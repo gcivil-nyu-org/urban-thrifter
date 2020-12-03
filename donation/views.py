fd7706fe7b2a59bd39fade89854a31dcf928d15e
Jonathan Pun <jonathan.pun88@gmail.com>
2020-12-03 03:59:28 -0500

    Formatting

--- donation/views.py
+++ donation/views.py
@@ -49,9 +49,11 @@ def home(request):
     available_donations = post_list.filter(
         status__in=["Available", "AVAILABLE"],
     )
-    available_donation_posts = (available_donations.filter(dropoff_time_1__gte=current_time) |
-        available_donations.filter(dropoff_time_2__gte=current_time) |
-        available_donations.filter(dropoff_time_3__gte=current_time))
+    available_donation_posts = (
+        available_donations.filter(dropoff_time_1__gte=current_time)
+        | available_donations.filter(dropoff_time_2__gte=current_time)
+        | available_donations.filter(dropoff_time_3__gte=current_time)
+    )
     closed_donation_posts = post_list.filter(status__in=["Closed", "CLOSED"])
     closed_reservation_posts = reserve_post_list.filter(
         reservationstatus=1, post__status__in=["Closed", "CLOSED"]
@@ -350,7 +352,8 @@ def get_reminder(request):
     # data = posts.count()
     # print(data)
     return render(request, "donation/messages.html", context)
-    
+
+
 def donation_expired(request):
     user = request.user
     current_time = timezone.now()
@@ -361,7 +364,7 @@ def donation_expired(request):
         dropoff_time_2__lt=current_time,
         dropoff_time_3__lt=current_time,
     ).order_by("-date_created")
-    
+
     page = request.GET.get("page", 1)
     paginator = Paginator(post_list, 5)
     try:
@@ -370,7 +373,7 @@ def donation_expired(request):
         expired_donation_posts = paginator.page(1)
     except EmptyPage:
         expired_donation_posts = paginator.page(paginator.num_pages)
-        
+
     context = {
         "expired_donation_posts": expired_donation_posts,
     }
